e103c04f7d54c54a191384681842ce165e6ece25
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2023-12-12 12:07:41 -0600

    started day 5

--- day5/a.go
+++ day5/a.go
@@ -1,1 +1,27 @@
 package day5
+
+type Mapper struct {
+	counter int
+	items   map[string]int
+}
+
+func (m *Mapper) GetItem(name string) int {
+	val, ok := m.items[name]
+	if ok {
+		return val
+	}
+
+	m.counter++
+	m.items[name] = m.counter
+
+	return m.counter
+}
+
+func NewMapper() *Mapper {
+	m := &Mapper{
+		counter: -1,
+		items:   map[string]int{},
+	}
+
+	return m
+}
