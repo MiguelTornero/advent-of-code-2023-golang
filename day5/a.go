fa2d8d356b0bcd26b0d93db13bd8d66e4d78eb13
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2023-12-16 14:11:47 -0600

    continued day 5

--- day5/a.go
+++ day5/a.go
@@ -1,5 +1,12 @@
 package day5
 
+import (
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 type Mapper struct {
 	counter int
 	items   map[string]int
@@ -11,17 +18,218 @@ func (m *Mapper) GetItem(name string) int {
 		return val
 	}
 
+	old := m.counter
 	m.counter++
-	m.items[name] = m.counter
+	m.items[name] = old
 
-	return m.counter
+	return old
 }
 
 func NewMapper() *Mapper {
 	m := &Mapper{
-		counter: -1,
+		counter: 0,
 		items:   map[string]int{},
 	}
 
 	return m
+}
+
+type Graph[T any] struct {
+	size   int
+	matrix []T
+}
+
+func NewGraph[T any](elements int) *Graph[T] {
+	if elements < 0 {
+		panic(errors.New("invalid graph size"))
+	}
+	mat := make([]T, elements*elements)
+	return &Graph[T]{
+		size:   elements,
+		matrix: mat,
+	}
+}
+
+func (g *Graph[T]) GetSize() int {
+	return g.size
+}
+
+func (g *Graph[T]) GetEdge(from int, to int) (T, error) {
+	var a T
+	if from < 0 || from >= g.size || to < 0 || to >= g.size {
+		return a, errors.New("index out of range")
+	}
+
+	index := from*g.size + to
+	a = g.matrix[index]
+
+	return a, nil
+}
+
+func (g *Graph[T]) SetEdge(from int, to int, value T) (T, error) {
+	var a T
+	if from < 0 || from >= g.size || to < 0 || to >= g.size {
+		return a, errors.New("index out of range")
+	}
+
+	index := from*g.size + to
+	g.matrix[index] = value
+
+	return value, nil
+}
+
+type Range struct {
+	start  int
+	end    int
+	offset int
+}
+
+func NewRange(start int, end int, offset int) *Range {
+	if end < start {
+		panic(errors.New("invalid range"))
+	}
+
+	return &Range{
+		start:  start,
+		end:    end,
+		offset: offset,
+	}
+}
+
+func (r *Range) InRange(n int) bool {
+	return n >= r.start && n <= r.end
+}
+
+func (r *Range) Transform(n int) int {
+	if r.InRange(n) {
+		return n + r.offset
+	}
+
+	return n
+}
+
+type RangeCollection struct {
+	ranges []*Range
+}
+
+func NewRangeCollection() *RangeCollection {
+	return &RangeCollection{
+		ranges: []*Range{},
+	}
+}
+
+func (rc *RangeCollection) InRange(n int) bool {
+	for _, r := range rc.ranges {
+		if r.InRange(n) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (rc *RangeCollection) GetSize() int {
+	return len(rc.ranges)
+}
+
+func (rc *RangeCollection) AddRage(start int, end int, offset int) *RangeCollection {
+	r := NewRange(start, end, offset)
+	rc.ranges = append(rc.ranges, r)
+
+	return rc
+}
+
+func (rc *RangeCollection) Transform(n int) int {
+	for _, r := range rc.ranges {
+		if r.InRange(n) {
+			return r.Transform(n)
+		}
+	}
+
+	return n
+}
+
+func (rc *RangeCollection) GetFirstMatchingRange(n int) *Range {
+	for _, r := range rc.ranges {
+		if r.InRange(n) {
+			return r
+		}
+	}
+
+	return nil
+}
+
+func ParseMapNums(lines []string, m *Mapper) (*RangeCollection, error) {
+	output := NewRangeCollection()
+	r := regexp.MustCompile(`\s+`)
+
+	FIELDS := 3 // a map string has three numbers
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		lineErr := errors.New("invalid map line: " + line)
+
+		if line == "" {
+			return output, nil
+		}
+
+		numsStr := r.Split(line, FIELDS+1)
+
+		if len(numsStr) != FIELDS {
+			return nil, lineErr
+		}
+
+		nums := make([]int, FIELDS)
+		for i, numStr := range numsStr {
+			num, err := strconv.Atoi(numStr)
+			if err != nil {
+				return nil, lineErr
+			}
+			nums[i] = num
+		}
+
+		start := nums[1]
+		end := nums[1] + nums[2] - 1
+		offset := nums[0]
+
+		output.AddRage(start, end, offset)
+
+	}
+
+	return output, nil
+}
+
+func ParseAlmanac(lines []string, maxElems int, m *Mapper) error {
+	seedsRegex := regexp.MustCompile(`^seeds:(.*)$`)
+	mapRegex := regexp.MustCompile(`^([A-Za-z]+)-to-([A-Za-z]+)\s+map:$`)
+
+	for i := 0; i < len(lines); i++ {
+		line := strings.TrimSpace(lines[i])
+		if line == "" {
+			continue // ignore empty lines
+		}
+
+		seedsMatch := seedsRegex.FindStringSubmatch(line)
+		if len(seedsMatch) > 0 {
+			// parse seed here
+			continue
+		}
+
+		mapMatch := mapRegex.FindStringSubmatch(line)
+		if len(mapMatch) > 0 {
+			rc, err := ParseMapNums(lines[i+1:], m)
+			if err != nil {
+				return err
+			}
+			size := rc.GetSize()
+			i = i + size
+
+			continue
+		}
+
+		return errors.New("invalid almanac string at line " + strconv.Itoa(i+1))
+
+	}
+
+	return nil
 }
---
+++ day5/a_test.go
@@ -0,0 +1,183 @@
+package day5_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/MiguelTornero/advent-of-code-2023-golang/day5"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMapper(t *testing.T) {
+	mapper := day5.NewMapper()
+
+	assert.Equal(t, mapper.GetItem("foo"), 0)
+
+	assert.Equal(t, mapper.GetItem("foo"), 0)
+
+	assert.Equal(t, mapper.GetItem("bar"), 1)
+
+	assert.Equal(t, mapper.GetItem("hello"), 2)
+
+	assert.Equal(t, mapper.GetItem("world"), 3)
+
+	assert.Equal(t, mapper.GetItem("bar"), 1)
+
+}
+
+func TestGraphInt(t *testing.T) {
+	g := day5.NewGraph[int](10)
+
+	assert.Equal(t, g.GetSize(), 10)
+
+	v, err := g.GetEdge(0, 1)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, v, 0)
+
+	_, err = g.SetEdge(0, 1, 32)
+
+	assert.Nil(t, err)
+
+	v, err = g.GetEdge(0, 1)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, v, 32)
+}
+
+func TestGraphErr(t *testing.T) {
+
+	assert.Panics(t, func() { day5.NewGraph[bool](-1) })
+
+	g := day5.NewGraph[bool](5)
+
+	assert.NotNil(t, g)
+
+	_, err := g.SetEdge(0, 100, false)
+
+	assert.NotNil(t, err)
+
+	_, err = g.SetEdge(100, 0, false)
+
+	assert.NotNil(t, err)
+
+	_, err = g.SetEdge(100, 100, false)
+
+	assert.NotNil(t, err)
+}
+
+func TestRange(t *testing.T) {
+	assert.Panics(t, func() { day5.NewRange(5, 2, -1) })
+
+	r := day5.NewRange(5, 10, -1)
+
+	assert.Equal(t, r.InRange(5), true)
+
+	assert.Equal(t, r.InRange(10), true)
+
+	assert.Equal(t, r.InRange(0), false)
+
+	assert.Equal(t, r.Transform(8), 7)
+
+	assert.Equal(t, r.Transform(1), 1)
+}
+
+func TestRangeCollection(t *testing.T) {
+	rc := day5.NewRangeCollection()
+
+	assert.Equal(t, rc.GetSize(), 0)
+
+	rc.AddRage(0, 9, -2)
+
+	rc.AddRage(20, 29, 2)
+
+	assert.Equal(t, rc.GetSize(), 2)
+
+	assert.Equal(t, rc.Transform(5), 3)
+
+	assert.Equal(t, rc.Transform(22), 24)
+
+	assert.Equal(t, rc.Transform(13), 13)
+
+	assert.Nil(t, rc.GetFirstMatchingRange(34))
+
+	assert.Equal(t, rc.GetFirstMatchingRange(7).Transform(7), rc.Transform(7))
+
+	assert.True(t, rc.InRange(8))
+
+	assert.True(t, rc.InRange(23))
+
+	assert.False(t, rc.InRange(12))
+
+	assert.Equal(t, rc.GetFirstMatchingRange(9).InRange(9), rc.InRange(9))
+
+	assert.Panics(t, func() { rc.AddRage(0, -1, 5) })
+}
+
+const almanacStr string = `seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`
+
+func TestParseMapNums(t *testing.T) {
+	input := []string{"1 2 3", "4 5 6", ""}
+	mapper := day5.NewMapper()
+
+	a, b := day5.ParseMapNums(input, mapper)
+
+	assert.NotNil(t, a)
+	assert.Nil(t, b)
+
+	r := a.GetFirstMatchingRange(2)
+	assert.NotNil(t, r)
+
+	r = a.GetFirstMatchingRange(5)
+	assert.NotNil(t, r)
+
+	assert.Equal(t, a.Transform(2), 3)
+	assert.Equal(t, a.Transform(4), 5)
+
+	assert.Equal(t, a.Transform(0), 0)
+
+	assert.Equal(t, a.Transform(5), 9)
+	assert.Equal(t, a.Transform(10), 14)
+}
+
+func TestAlmanacParser(t *testing.T) {
+	lines := strings.Split(almanacStr, "\n")
+	mapper := day5.NewMapper()
+
+	err := day5.ParseAlmanac(lines, 10, mapper)
+	assert.Nil(t, err)
+}
